01487f9ddffe720573be4a0115947301e4c911f8
Michael Wohl <mike@gatorriders.com>
2016-01-29 22:53:57 -0500

    created gradiant nav bar and bird image

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express'),
     server = express();
 
+server.use(express.static(__dirname+'/css'));
+server.use(express.static(__dirname+'/images'));
+
 server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
+});
+
+server.post('/signup', function(req, res){
+  console.log('You signed up');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 server.listen(8080, function(){
